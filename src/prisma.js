231309af74e262661a04d4ee1cea710dd0544481
Kunark Khewal <kunark.khewal88@gmail.com>
2020-08-16 18:26:44 +0530

    dotenv is added and secret is create for authorising prisma routes

--- src/prisma.js
+++ src/prisma.js
@@ -1,8 +1,11 @@
 import { Prisma } from 'prisma-binding';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
-    endpoint: 'http://localhost:4466'
+    endpoint: 'http://localhost:4466',
+    secret: process.env.SECRET
 })
 
 export { prisma as default }
