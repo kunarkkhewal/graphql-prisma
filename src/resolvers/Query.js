c4b67c602ca57111ee29717c20141f95f83d1f4f
Kunark Khewal <kunark.khewal88@gmail.com>
2020-08-22 19:19:45 +0530

    pagination and sorting applied

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,7 +2,12 @@ import getUserId from '../utils/getUserId';
 
 const Query = {
     users(parent, args, { prisma }, info) {
-        const opArgs = {};
+        const opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
+            orderBy: args.orderBy
+        };
         if(args.query){
             opArgs.where = {
                 OR:[{
@@ -18,6 +23,10 @@ const Query = {
     myPosts(parent, args, { prisma,request }, info){
         const userId = getUserId(request)
         const opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
+            orderBy: args.orderBy,
             where: {
                 author:{
                     id: userId
@@ -37,6 +46,10 @@ const Query = {
 
     posts(parent, args, { prisma }, info) {
         const opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
+            orderBy: args.orderBy,
             where: {
                 published: true
             }
@@ -52,7 +65,13 @@ const Query = {
     },
 
     comments(parent, args, { prisma }, info) {
-        return prisma.query.comments(null, info)
+        const opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
+            orderBy: args.orderBy
+        }
+        return prisma.query.comments(opArgs, info)
     },
 
     add(parent, args) {
